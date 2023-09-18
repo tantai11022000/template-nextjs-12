98c1d937a7426d9104117834df8e60f5a162f4d4
tho_huynh <tho_huynh@enhance.vn>
2023-09-18 15:34:05 +0700

    update skeleton

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import React from 'react';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
+);
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,29 @@
+import React, { ReactChild, ReactFragment, ReactPortal } from 'react';
+import Header from '../header';
+import Footer from '../footer';
+
+type ReactNode =
+  | ReactChild
+  | ReactFragment
+  | ReactPortal
+  | boolean
+  | null
+  | undefined;
+
+interface Props {
+  children: ReactNode;
+}
+
+function Layout(props: Props) {
+  const { children } = props;
+
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
