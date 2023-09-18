2a823eb367f636a4f06b7c9a57a2ef13cbabba38
tho_huynh <tho_huynh@enhance.vn>
2023-09-18 16:28:10 +0700

    update skeleton

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,36 @@
 'use client'
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 const { Header, Content, Footer } = Layout;
+
+const menuItems = [
+    { label: `Home`, url: `/` },
+    { label: `Dashboard`, url: `/dashboard/analytics` },
+    { label: `Newsletter`, url: `/newsletter` },
+];
+
+const ActiveMenuLink = ({ children, href }: any) => {
+    const pathname = usePathname();
+    const active = href === pathname;
+  
+    return (
+      <Link
+        href={href}
+        className={`hover:bg-gray-100 p-2 rounded block ${
+          active ||
+          (href.startsWith('/dashboard') &&
+            pathname.startsWith('/dashboard'))
+            ? 'text-black font-semibold'
+            : 'text-gray-500'
+        }`}
+      >
+        {children}
+      </Link>
+    );
+  };
 
 function HeaderApp() {
   const {
@@ -10,27 +39,36 @@ function HeaderApp() {
 
   return (
     <Layout>
-      <Header
-        style={{
-          position: 'sticky',
-          top: 0,
-          zIndex: 1,
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={new Array(3).fill(null).map((_, index) => ({
-            key: String(index + 1),
-            label: `nav ${index + 1}`,
-          }))}
-        />
-      </Header>
+      <header className="flex flex-col gap-5">
+      <div className="py-4 flex items-center">
+        <Link href="/">
+          <Image
+            width={36}
+            height={36}
+            src="/favicon.ico"
+            className="w-8 md:w-9"
+            alt="logo"
+          />
+        </Link>
+        <nav className="ml-8">
+          <ul className="flex flex-wrap gap-x-8 text-gray-900">
+            {menuItems.map(({ url, label }, index) => (
+              <li key={index}>
+                <ActiveMenuLink href={url}>{label}</ActiveMenuLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      {/* <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="block w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:border-gray-500"
+        placeholder="Search..."
+      /> */}
+    </header>
       {/* <Content className="site-layout" style={{ padding: '0 50px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
