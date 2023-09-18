45dcc9e3da6f8808167803b76dfbe7d567f8f0f7
tho_huynh <tho_huynh@enhance.vn>
2023-09-18 16:06:08 +0700

    update skeleton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,12 @@ import Layout from '@/components/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+export default function App({ Component, pageProps }: any) {
+  const renderWithLayout =
+    Component.getLayout ||
+    function (page: any) {
+      return <Layout>{page}</Layout>;
+    };
 
-  )
+  return renderWithLayout(<Component {...pageProps} />);
 }
