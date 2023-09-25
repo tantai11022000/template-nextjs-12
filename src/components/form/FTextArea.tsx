0a3f263b7e8d9847168d7c9526990dac724b6421
Trong Bao <vutrongbao.trinh.ltb@gmail.com>
2023-09-25 10:56:00 +0700

    form field components

---
+++ src/components/form/FTextArea.tsx
@@ -0,0 +1,26 @@
+import { Form, Input } from 'antd';
+import * as React from 'react';
+
+export interface IFTextAreaProps {
+  name: string,
+  label: string,
+  required?: boolean,
+  errorMessage?: string,
+  rows?: number
+}
+
+export default function FTextArea (props: IFTextAreaProps) {
+  const {name, label, required, errorMessage, rows} = props
+  return (
+    <Form.Item
+      name={name}
+      label={label}
+      rules={[{
+        required: required ? true : false, 
+        message: errorMessage ? errorMessage : 'Please input this field',
+      }]}
+    >
+      <Input.TextArea rows={rows ? rows : 5} />
+    </Form.Item>
+  );
+}
