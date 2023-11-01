ba53f0e618e20aee8ed6af69d8db0bcb2173b180
tai_nguyen <tai_nguyen@enhance.vn>
2023-11-01 10:47:06 +0700

    fix authen

--- src/utils/StorageKeys.ts
+++ src/utils/StorageKeys.ts
@@ -1,13 +1,1 @@
 export const TOKEN_KEY = 'viewer';
-export const REFRESH_TOKEN_KEY = 'loyatyRefreshToken';
-export const OPENLIVE_TOKEN_KEY = 'accessToken';
-export const OPENLIVE_REFRESH_TOKEN_KEY = 'refreshToken';
-export const USER_DATA_KEY = 'views';
-export const PERMISSION_DATA_KEY = 'permissionData';
-export const SHIPPING_ADDRESS = 'shippingAddress';
-
-export const LANGUAGE_KEY = 'language';
-export const LOGIN_REFERER = 'loginReferer'
-export const REMEMBER_ME = 'rememberMe'
-
-export const CURRENT_MENU = 'currentMenu'
