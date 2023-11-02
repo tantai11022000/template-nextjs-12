d8c09186bb44f2129236c44bb045493177edceba
tai_nguyen <tai_nguyen@enhance.vn>
2023-11-02 17:18:46 +0700

    fix env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   basePath: process.env.NEXT_PUBLIC_SUB_URL ? process.env.NEXT_PUBLIC_SUB_URL : null,
-  assetPrefix: process.env.NEXT_PUBLIC_SUB_URL ? process.env.NEXT_PUBLIC_SUB_URL : null,
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX ? process.env.NEXT_PUBLIC_ASSET_PREFIX : null,
 }
 
 module.exports = nextConfig
