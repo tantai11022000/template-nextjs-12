a9e8e8e12f1c488aaa3c6a1afa53b69eda67741f
tai_nguyen <tai_nguyen@enhance.vn>
2023-11-03 09:20:29 +0700

    fix config path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   basePath: process.env.NEXT_PUBLIC_SUB_URL ? process.env.NEXT_PUBLIC_SUB_URL : null,
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX ? process.env.NEXT_PUBLIC_ASSET_PREFIX : null,
+  assetPrefix: process.env.NEXT_PUBLIC_SUB_URL ? process.env.NEXT_PUBLIC_SUB_URL : null,
+  async rewrites(){
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_SUB_URL}/_next/:path*`,
+        destination: '_next/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
