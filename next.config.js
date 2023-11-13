75f15c1eb0332144fb2629a26838d1f952ca9a3d
Trong Bao <vutrongbao.trinh.ltb@gmail.com>
2023-11-13 16:13:32 +0700

    edit function getstaticprops and config i18n

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
-  reactStrictMode: false,
-  assetPrefix: '/amazon',
-  basePath: '/amazon',
+  reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_SUB_URL ? process.env.NEXT_PUBLIC_SUB_URL : null,
+  // assetPrefix: process.env.NEXT_PUBLIC_SUB_URL ? process.env.NEXT_PUBLIC_SUB_URL : null,
   i18n,
 }
 
