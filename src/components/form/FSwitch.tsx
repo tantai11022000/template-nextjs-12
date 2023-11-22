6ab16cb013fd826d1ad3fd39e38e9400ed235958
Trong Bao <vutrongbao.trinh.ltb@gmail.com>
2023-11-22 13:39:11 +0700

    fix issue MD-2346

--- src/components/form/FSwitch.tsx
+++ src/components/form/FSwitch.tsx
@@ -5,11 +5,13 @@ export interface IFSwitchProps {
   name: string,
   label: string,
   required?: boolean,
-  errorMessage?: string
+  errorMessage?: string,
+  status: boolean,
+  disable?: boolean
 }
 
 export default function FSwitch (props: IFSwitchProps) {
-  const {name, label, required, errorMessage} = props
+  const {name, label, required, errorMessage, status, disable} = props
   return (
     <Form.Item 
       label={label}
@@ -18,7 +20,9 @@ export default function FSwitch (props: IFSwitchProps) {
         required: required ? true : false, 
         message: errorMessage ? errorMessage : 'This field must be ON mode',
       }]}
-      valuePropName={name}
+      initialValue={status}
+      valuePropName={status ? "checked" : ""}
+      style={{display: disable ? "none" : ""}}
     >
       <Switch/>
     </Form.Item>
